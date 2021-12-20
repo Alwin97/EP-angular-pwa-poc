9bfdc6a3cece8ef384c7f03c0a5f8b8016530fc0
Alwin97 <2111Am1997>
2021-12-20 04:35:19 +0100

    - changed model a little bit

--- src/app/models/training.model.ts
+++ src/app/models/training.model.ts
@@ -1,14 +1,17 @@
 import {Team} from "./team.model";
 import {MyEvent} from "./event.model";
 import {ExercisePlan} from "./exercisePlan.model";
+import {User} from "./user.model";
 
 export class Training extends MyEvent {
   team?: Team;
+  trainer?: User;
   exercisePlan?: ExercisePlan;
 
   constructor(training: Training) {
     super(training);
     this.team = training.team;
+    this.trainer = training.trainer;
     this.exercisePlan = training.exercisePlan;
   }
 }
